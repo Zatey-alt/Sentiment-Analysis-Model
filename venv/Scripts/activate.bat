7925f152bd8f7bd4759ee67cda6324cc1982bb01
Zatey <michael.zatey@kibo.school>
2023-12-10 13:35:08 +0000

    add rep

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\micha\Desktop\Model_Deployment\venv
+set VIRTUAL_ENV=C:\Users\micha\Desktop\Sentiment Analysis Model\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
