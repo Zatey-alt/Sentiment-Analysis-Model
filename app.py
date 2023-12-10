2029fda58fcff37837fc58a6c04fafb5e0810e89
Zatey <michael.zatey@kibo.school>
2023-12-10 09:46:16 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+
+app = Flask(__name__)
+
+# Load the saved model
+model = joblib.load('sentiment_model.pkl')
+# vectorizer = CountVectorizer()
+vectorizer = joblib.load('vectorizer.pkl') 
+
+# create a route that manages user request and does sentiment prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        text = data['text']
+        
+        vectorized_text = vectorizer.transform([text])
+        
+        prediction = int(model.predict(vectorized_text)[0])
+        return jsonify({'sentiment': prediction})
+    
+    except Exception as e:
+        print('Exception in predict method, Something went wrong!: ', e)
+        return jsonify({'Error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
