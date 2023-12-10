48b1efe16c19a1dbd8a7ef5c21f8d02268ac7e8c
Zatey <michael.zatey@kibo.school>
2023-12-10 09:53:00 +0000

    remove script

--- app.py
+++ app.py
@@ -24,6 +24,3 @@ def predict():
     except Exception as e:
         print('Exception in predict method, Something went wrong!: ', e)
         return jsonify({'Error': str(e)})
-
-if __name__ == '__main__':
-    app.run(debug=True)
